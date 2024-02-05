9ac49d45185e0f03379c18b8a32c47ef602d9855
Elton Carreiro <eltoonx3@gmail.com>
2024-02-05 18:20:15 -0300

    Add coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,15 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const jestConfig: JestConfigWithTsJest = {
   // [...]
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        // ts-jest configuration goes here
-      }
-    ]
+    '^.+\\.tsx?$': 'ts-jest'
   },
+  cacheDirectory: '<rootDir>/.jest/cache',
+  collectCoverage: true,
+  coverageReporters: ['text', 'html', 'clover', 'lcov', 'json'],
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   }
