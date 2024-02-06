c60e592e47ea2ec439773e5046ef8be27405626d
Elton Carreiro <eltoonx3@gmail.com>
2024-02-05 23:53:11 -0300

    Adjust test to achieve 100% coverage on entities

--- src/entities/__tests__/Pessoa.test.ts
+++ src/entities/__tests__/Pessoa.test.ts
@@ -1,26 +1,44 @@
 import { Pessoa, TipoPessoaEmpresa } from '../Pessoa';
 
 describe('Pessoa testes', () => {
-  it('não deve permitir criar pessoa com código vazio', () => {
-    expect(() => criarPessoaFake({ codigo: '' })).toThrow(
-      'Código é obrigatório.'
-    );
-  });
+  describe('ao criar uma pessoa', () => {
+    it('não deve permitir criar pessoa com código vazio', () => {
+      expect(() => criarPessoaFake({ codigo: '' })).toThrow(
+        'Código é obrigatório.'
+      );
+    });
 
-  it('não deve permitir criar pessoa com nome vazio', () => {
-    expect(() => criarPessoaFake({ nome: '' })).toThrow('Nome é obrigatório.');
-  });
+    it('não deve permitir criar pessoa com nome vazio', () => {
+      expect(() => criarPessoaFake({ nome: '' })).toThrow(
+        'Nome é obrigatório.'
+      );
+    });
 
-  it('não deve permitir criar pessoa com sobrenome vazio', () => {
-    expect(() => criarPessoaFake({ sobrenome: '' })).toThrow(
-      'Sobrenome é obrigatório.'
-    );
-  });
+    it('não deve permitir criar pessoa com sobrenome vazio', () => {
+      expect(() => criarPessoaFake({ sobrenome: '' })).toThrow(
+        'Sobrenome é obrigatório.'
+      );
+    });
 
-  it('não deve permitir criar pessoa com código da empresa vazio', () => {
-    expect(() => criarPessoaFake({ codigoEmpresa: '' })).toThrow(
-      'Código da empresa inválido.'
-    );
+    it('não deve permitir criar pessoa com código da empresa vazio', () => {
+      expect(() => criarPessoaFake({ codigoEmpresa: '' })).toThrow(
+        'Código da empresa inválido.'
+      );
+    });
+
+    it('deve criar pessoa com dados corretos', () => {
+      const args = criarArgumentosPessoaFake({
+        tipoPessoaEmpresa: undefined
+      });
+      const pessoa = criarPessoaFake(args);
+
+      expect(pessoa.codigo).toBe(args.codigo);
+      expect(pessoa.codigoEmpresa).toBe(args.codigoEmpresa);
+      expect(pessoa.cpf.value).toBe(args.cpf);
+      expect(pessoa.nome).toBe(args.nome);
+      expect(pessoa.sobrenome).toBe(args.sobrenome);
+      expect(pessoa.tipoPessoaEmpresa).toBe(undefined);
+    });
   });
 });
 
