fa79e71a7a8fb78b352693da39cb9a03522dd24d
Elton Carreiro <eltoonx3@gmail.com>
2024-02-08 23:44:22 -0300

    Remove pseudo tty allocation

--- scripts/test_coverage.sh
+++ scripts/test_coverage.sh
@@ -1,3 +1,3 @@
 docker-compose up -d --build
-docker-compose exec lugo-api npx tsx src/migrate.ts
-docker-compose exec lugo-api npx jest --testRegex=\\.test\\.ts$ -w 1 $@ && docker-compose down -v
+docker-compose exec -T lugo-api npx tsx src/migrate.ts
+docker-compose exec -T lugo-api npx jest --testRegex=\\.test\\.ts$ -w 1 $@ && docker-compose down -v
