bf41776a94704221a45e7dbd5df57c881227e37d
Elton Carreiro <eltoonx3@gmail.com>
2024-02-20 20:42:14 -0300

    Add imovel creation mutations

--- src/graphql/schema/imovel.ts
+++ src/graphql/schema/imovel.ts
@@ -15,4 +15,68 @@ export const Imovel = builder.objectRef<ImovelType>('Imovel').implement({
   })
 });
 
-// TODO: Criar e alterar imóvel
+builder.mutationField('criarImovel', (t) =>
+  t.field({
+    type: Imovel,
+    description: 'Cria um imóvel',
+    args: {
+      endereco: t.arg.string({ required: true }),
+      metrosQuadrados: t.arg.int({ required: true }),
+      codigoDono: t.arg.string()
+    },
+    resolve: async (_, { endereco, metrosQuadrados, codigoDono }, ctx) => {
+      const codigoUsuarioSolicitante = ctx.usuarioLogado?.codigo;
+
+      if (!codigoUsuarioSolicitante) {
+        throw new Error('Usuário não autenticado');
+      }
+
+      const codigoImovel = await ctx.useCases.imovel.cadastrarImovel({
+        codigoUsuarioSolicitante,
+        endereco,
+        metrosQuadrados,
+        codigoDono: codigoDono ?? undefined
+      });
+
+      // TODO: Make routine return whole data rather than just code, this can lead to bugs
+      return {
+        codigo: codigoImovel,
+        endereco,
+        metrosQuadrados
+      };
+    }
+  })
+);
+
+builder.mutationField('alterarImovel', (t) =>
+  t.field({
+    type: Imovel,
+    description: 'Altera um imóvel',
+    args: {
+      codigoImovel: t.arg.string({ required: true }),
+      endereco: t.arg.string({ required: true }),
+      metrosQuadrados: t.arg.int({ required: true })
+    },
+    resolve: async (_, { codigoImovel, endereco, metrosQuadrados }, ctx) => {
+      const codigoUsuarioSolicitante = ctx.usuarioLogado?.codigo;
+
+      if (!codigoUsuarioSolicitante) {
+        throw new Error('Usuário não autenticado');
+      }
+
+      await ctx.useCases.imovel.alterarImovel({
+        codigoUsuarioSolicitante,
+        codigoImovel,
+        endereco,
+        metrosQuadrados
+      });
+
+      // TODO: Make routine return whole data rather than just code, this can lead to bugs
+      return {
+        codigo: codigoImovel,
+        endereco,
+        metrosQuadrados
+      };
+    }
+  })
+);
